3dd8b84cc792435f8cd269ae716afbc43b3714cb
Michael Rog <michaelrog@users.noreply.github.com>
2011-07-07 11:42:42 -0500

    hotfix: docs URL typo. also adding version number into readme.

--- ee2/system/third_party/show_me_my_assets/ext.show_me_my_assets.php
+++ ee2/system/third_party/show_me_my_assets/ext.show_me_my_assets.php
@@ -52,7 +52,7 @@ class Show_me_my_assets_ext
 	var $version = ROGEE_SMMA_VERSION ;
 	var $description = "Redirects the CP menu link for the EE File Manager with the Assets File Browser" ;
 	var $settings_exist = "n" ;
-	var $docs_url = "http//rog.ee/show_me_my_assets" ;
+	var $docs_url = "http://rog.ee/show_me_my_assets" ;
 	
 	
 	/**
